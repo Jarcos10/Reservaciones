079be9f560857f3e8d2bc24d42f2854e60e4bf19
Julio <julio.arcos.salazar@gmail.com>
2026-06-22 01:29:57 -0600

    Modificando configuraciones del proyecto para conexión en bd de yugabyte y CORS

--- Reservaciones/src/main/java/org/example/reservaciones/core/config/CorsConfig.java
+++ Reservaciones/src/main/java/org/example/reservaciones/core/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package org.example.reservaciones.core.config;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,16 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
-                                "http://localhost:4200",
-                                "http://127.0.0.1:4200"
-                        )
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Content-Disposition")
